76aae51b5b418a2be5a1c5c1a549a76c53f86cca
nitrocon <77290402+nitrocon@users.noreply.github.com>
2025-03-10 14:00:43 +0100

    Add files via upload

---
+++ themes/themesgui.py
@@ -0,0 +1,90 @@
+THEMES = {
+    "dark": """
+        QWidget {
+            background-color: #2e2e2e;
+            color: #D8DEE9;
+        }
+        QPushButton {
+            background-color: #3e3e3e;
+            color: #D8DEE9;
+            border: 1px solid #4e4e4e;
+            padding: 5px;
+        }
+        QPushButton:hover {
+            background-color: #4e4e4e;
+        }
+        QLineEdit {
+            background-color: #4e4e4e;
+            color: #D8DEE9;
+            border: 1px solid #3e3e3e;
+            padding: 5px;
+        }
+        QTextEdit {
+            background-color: #3e3e3e;
+            color: #D8DEE9;
+            border: 1px solid #4e4e4e;
+        }
+        QProgressBar {
+            background-color: #2e2e2e;
+            color: #D8DEE9;
+            border: 1px solid #4e4e4e;
+            text-align: center;
+        }
+        QProgressBar::chunk {
+            background-color: #4e4e4e;
+        }
+        QComboBox {
+            background-color: #3e3e3e;
+            color: #D8DEE9;
+            border: 1px solid #4e4e4e;
+            padding: 5px;
+        }
+        QLabel {
+            color: #D8DEE9;
+        }
+    """,
+    "light": """
+        QWidget {
+            background-color: #ECEFF4;
+            color: #2E3440;
+        }
+        QPushButton {
+            background-color: #D8DEE9;
+            color: #2E3440;
+            border: 1px solid #81A1C1;
+            padding: 5px;
+        }
+        QPushButton:hover {
+            background-color: #81A1C1;
+        }
+        QLineEdit {
+            background-color: #D8DEE9;
+            color: #2E3440;
+            border: 1px solid #81A1C1;
+            padding: 5px;
+        }
+        QTextEdit {
+            background-color: #D8DEE9;
+            color: #2E3440;
+            border: 1px solid #81A1C1;
+        }
+        QProgressBar {
+            background-color: #D8DEE9;
+            color: #2E3440;
+            border: 1px solid #81A1C1;
+            text-align: center;
+        }
+        QProgressBar::chunk {
+            background-color: #81A1C1;
+        }
+        QComboBox {
+            background-color: #D8DEE9;
+            color: #2E3440;
+            border: 1px solid #81A1C1;
+            padding: 5px;
+        }
+        QLabel {
+            color: #2E3440;
+        }
+    """
+}
